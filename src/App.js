46b014cea22157c398f7bb22ed3b8b2fd8714458
Julian Kraemer <juliankraemer184@gmail.com>
2023-05-11 13:12:12 +0200

    support customized prompts

--- src/App.js
+++ src/App.js
@@ -13,6 +13,23 @@ function App() {
     },
   ]);
   const [isTyping, setIsTyping] = useState(false);
+  const [customPrompt, setCustomPrompt] = useState("");
+
+  const handleUpdatePrompt = () => {
+    if (customPrompt === "") {
+      return;
+    }
+    sessionStorage.setItem("customPrompt", customPrompt);
+  };
+
+  const handleResetPrompt = () => {
+    setCustomPrompt("");
+    sessionStorage.removeItem("customPrompt");
+  };
+
+  const handleCustomPromptChange = (event) => {
+    setCustomPrompt(event.target.value);
+  };
 
   const handleNewMessage = (message) => {
     setMessages((prevMessages) => [...prevMessages, message]);
@@ -38,15 +55,25 @@ function App() {
     setIsTyping(true);
     e.target.reset();
 
+    const promptExtension = sessionStorage.getItem("customPrompt");
+
+    let requestBody = {};
+    if (promptExtension === null || promptExtension === "") {
+      requestBody = { query: newMessage.content };
+    } else {
+      requestBody = {
+        query: newMessage.content,
+        promptExtension: promptExtension,
+      };
+    }
+
     const response = await fetch("https://fuergruender-stream.fly.dev/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "text/event-stream",
       },
-      body: JSON.stringify({
-        query: newMessage.content,
-      }),
+      body: JSON.stringify(requestBody),
     });
     if (response.status === 200) {
       let newMessage = { content: "", role: "assistant" };
@@ -68,73 +95,123 @@ function App() {
       console.error("error getting resource", response.status);
     }
   };
-
   return (
-    <section className="container mx-auto p-5 fixed inset-0">
-      <div className="mockup-window border bg-base-300 w-full h-full flex flex-col">
-        <div className="p-5 pb-8 flex-grow overflow-auto">
-          {messages.length &&
-            messages.map((msg, i) => {
-              return (
-                <div
-                  className={`chat ${
-                    msg.role === "assistant" ? "chat-start" : "chat-end"
-                  }`}
-                  key={"chatKey" + i}
-                >
-                  <div className="chat-image avatar">
-                    <div className="w-10 rounded-full">
-                      <img
-                        src={
-                          msg.role === "assistant"
-                            ? "/images/gruenderheld.png"
-                            : "/images/fuergruender.png"
-                        }
-                      />
+    <div className="drawer">
+      <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <section className="container mx-auto p-5 fixed inset-0">
+          <div className="mockup-window border bg-base-300 w-full h-full flex flex-col">
+            <div className="p-5 pb-8 flex-grow overflow-auto">
+              {messages.length &&
+                messages.map((msg, i) => {
+                  return (
+                    <div
+                      className={`chat ${
+                        msg.role === "assistant" ? "chat-start" : "chat-end"
+                      }`}
+                      key={"chatKey" + i}
+                    >
+                      <div className="chat-image avatar">
+                        <div className="w-10 rounded-full">
+                          <img
+                            src={
+                              msg.role === "assistant"
+                                ? "/images/gruenderheld.png"
+                                : "/images/fuergruender.png"
+                            }
+                          />
+                        </div>
+                      </div>
+                      <div className="chat-bubble">
+                        <Linkify as="p" options={linkifyOptions}>
+                          {msg.content}
+                        </Linkify>
+                      </div>
                     </div>
-                  </div>
-                  <div className="chat-bubble">
-                    <Linkify as="p" options={linkifyOptions}>
-                      {msg.content}
-                    </Linkify>
-                  </div>
-                </div>
-              );
-            })}
-        </div>
+                  );
+                })}
+            </div>
 
-        <form
-          className="form-control m-5 items-center"
-          onSubmit={(e) => handleSubmit(e)}
-        >
-          <div className="input-group max-w-full w-[800px] relative">
-            {isTyping && (
-              <small className="absolute -top-5 left-0.5 animate-pulse">
-                Gründerbot ist am tippen...
-              </small>
-            )}
+            <form
+              className="form-control m-5 items-center"
+              onSubmit={(e) => handleSubmit(e)}
+            >
+              <div className="input-group max-w-full w-[800px] relative">
+                {isTyping && (
+                  <small className="absolute -top-5 left-0.5 animate-pulse">
+                    Gründerbot ist am tippen...
+                  </small>
+                )}
+                <label
+                  htmlFor="my-drawer-4"
+                  className="drawer-button btn btn-square"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="25"
+                    height="25"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="3" y1="12" x2="21" y2="12"></line>
+                    <line x1="3" y1="6" x2="21" y2="6"></line>
+                    <line x1="3" y1="18" x2="21" y2="18"></line>
+                  </svg>
+                </label>
 
-            <input
-              class="font-gruender"
-              type="text"
-              placeholder="Stellen Sie Ihre Frage"
-              className="input input-bordered flex-grow"
-              required
-            />
-            <button className="btn btn-square" type="submit">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-              </svg>
-            </button>
+                <input
+                  type="text"
+                  placeholder="Stellen Sie Ihre Frage"
+                  className="input input-bordered flex-grow font-gruender"
+                  required
+                />
+                <button className="btn btn-square" type="submit">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                  </svg>
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
+        </section>
       </div>
-    </section>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+          <li>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Benutzerdefinierter Prompt</span>
+              </label>
+              <textarea
+                onChange={handleCustomPromptChange}
+                value={customPrompt}
+                className="textarea textarea-bordered h-40"
+                placeholder="Beispiel: Deine Antwort sollte ausführlich und professionell sein."
+              ></textarea>
+              <button onClick={handleUpdatePrompt} className="btn btn-sm">
+                update Prompt
+              </button>
+              <button
+                onClick={handleResetPrompt}
+                className="btn btn-sm btn-secondary"
+              >
+                reset prompt
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
